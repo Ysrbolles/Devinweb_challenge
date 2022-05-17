6ae760cddb1863906d6a1650172dd3719e53afc1
ybolles <ysrbolles@gmail.com>
2022-05-17 19:04:16 +0100

    ⬆️ Change Navbar UI

--- client/src/components/Layout/Navbar.vue
+++ client/src/components/Layout/Navbar.vue
@@ -1,32 +1,31 @@
 <template>
-  <div
-    class="bg-gray-900 text-gray-100 py-8 px-6 rounded-b-3xl shadow md:flex justify-between items-center"
-  >
-    <div class="flex items-center cursor-pointer hover:text-gray-300">
-      <span class="text-gray-100 hover:text-gray-300 text-xl mr-1">
-        <i class="bi bi-shop text-2xl" />
-      </span>
-      <h1 class="text-3xl">DevInWeb</h1>
-    </div>
-    <span @click="openmenu()" class="absolute md:hidden right-6 top-8 cursor-pointer text-4xl" >
-        <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']" />
-    </span>
-    <ul
-      class="md:flex md:items-center md:px-0 px-8 md:pb-0 pb-8 md:static absolute bg-gray-900  rounded-b-3xl md:w-auto w-full top-19 duration-700 ease-in"
-      :class="[open ? 'left-0' : 'left-[-100%]']"
+  <header class="text-gray-600 body-font">
+    <div
+      class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
     >
-      <li class="md:mx-4 md:my-0 my-6" v-for="link in Links" :key="link.name">
+      <a
+        class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+      >
+        <span>
+          <i class="bi bi-shop text-2xl" />
+        </span>
+        <span class="ml-3 text-3xl">Devinweb</span>
+      </a>
+      <nav
+        class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
+      >
         <a
-          :href="link.link"
-          class="text-xl hover:text-gray-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+          class="mr-5 hover:text-gray-900 text-xl"
+          v-for="link in Links"
+          :key="link.name"
           >{{ link.name }}</a
         >
-      </li>
-      <span class="cursor-pointer hover:text-gray-300">
-          <li class="bi bi-bag-heart-fill text-2xl"/>
+      </nav>
+      <span class="cursor-pointer">
+        <i class="bi bi-bag-heart-fill text-2xl text-gray-900" />
       </span>
-    </ul>
-  </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -42,10 +41,10 @@ export default {
       { name: "About", link: "#" },
       { name: "Contact", link: "#" },
     ];
-    function openmenu () {
-        open.value = !open.value;
+    function openmenu() {
+      open.value = !open.value;
     }
-    return { Links, open , openmenu};
+    return { Links, open, openmenu };
   },
 };
 </script>
