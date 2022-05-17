de22b3bfe4be76652ff3ea89ca14c23cae5dc11d
ybolles <ysrbolles@gmail.com>
2022-05-17 16:10:52 +0100

    ✨ Create Navbar Section

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,33 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <Navbar />
+  </div>
 </template>
+
+<script>
+import Navbar from "./components/Layout/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
 }
 </style>
---
+++ client/src/components/Layout/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="bg-gray-900 text-gray-100 py-8 px-6 rounded-b-3xl shadow md:flex justify-between items-center"
+  >
+    <div class="flex items-center cursor-pointer hover:text-gray-300">
+      <span class="text-gray-100 hover:text-gray-300 text-xl mr-1">
+        <i class="bi bi-shop-window text-2xl" />
+      </span>
+      <h1 class="text-3xl">DevInWeb</h1>
+    </div>
+    <span @click="openmenu()" class="absolute md:hidden right-6 top-6 cursor-pointer text-4xl" >
+        <i :class="[open ? 'bi bi-x' : 'bi bi-filter-left']" />
+    </span>
+    <ul
+      class="md:flex md:items-center md:px-0 px-5 md:pb-0 pb-5 md:static absolute bg-gray-900  rounded-b-3xl md:w-auto w-full top-19 duration-700 ease-in"
+      :class="[open ? 'left-0' : 'left-[-100%]']"
+    >
+      <li class="md:mx-4 md:my-0 my-6" v-for="link in Links" :key="link.name">
+        <a
+          :href="link.link"
+          class="text-xl hover:text-gray-300 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+          >{{ link.name }}</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+
+export default {
+  name: "Navbar",
+  setup() {
+    let open = ref(false);
+    let Links = [
+      { name: "Home", link: "#" },
+      { name: "About", link: "#" },
+      { name: "Categories", link: "#" },
+      { name: "Products", link: "#" },
+    ];
+    function openmenu () {
+        open.value = !open.value;
+    }
+    return { Links, open , openmenu};
+  },
+};
+</script>
